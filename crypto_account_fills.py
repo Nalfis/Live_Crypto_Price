7611598e3e42fa0a772d2141c5b5120f90089e11
Nalfis Tobar <nalfis.tobar@gmail.com>
2017-10-16 21:15:11 -0400

    "added price and quantity calculations by date"

--- crypto_account_fills.py
+++ crypto_account_fills.py
@@ -6,7 +6,7 @@
 #
 #   easy_install requests
 
-import json, hmac, hashlib, time, requests, base64, yaml
+import json, hmac, hashlib, time, requests, base64, yaml, datetime
 from requests.auth import AuthBase
 
 # Create custom authentication for Exchange
@@ -38,24 +38,71 @@ class CoinbaseExchangeAuth(AuthBase):
 with open("crypto.yaml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
-
 api_url = cfg['url']['gdax']
 API_KEY = cfg['auth']['api_key']
 API_SECRET = cfg['auth']['api_sec']
 API_PASS = cfg['auth']['pass_ph']
 auth = CoinbaseExchangeAuth(API_KEY, API_SECRET, API_PASS)
 
-# Get accounts
-fills = requests.get(api_url + 'fills', auth=auth)
-list_fills = fills.json()
-print json.dumps(list_fills, indent=4, sort_keys=True)
+
+def get_current_fill(days=0):
+    # Get accounts
+    fills = requests.get(api_url + 'fills', auth=auth)
+    list_fills = fills.json()
+    LTC = list_fills
+    day = days * - 1
+    fill_date = (datetime.datetime.now() + datetime.timedelta(days=day)).strftime('%Y-%m-%d')
+    buy_sum_size = 0
+    sell_sum_size = 0
+    buy_fee = 0
+    sell_fee = 0
+    buy_sum_price = 0
+    sell_sum_price = 0
+
+    for k in LTC:
+        if k['product_id'] == "LTC-USD" and k['created_at'][:10] == fill_date and k['side'] == 'buy':
+            print "side: {} - size: {} * price ${:,.5f} fee: ${:,.5f} - date: {}".format(k['side'],
+                                                                                         k['size'],
+                                                                                         float(k['price']),
+                                                                                         float(k['fee']),
+                                                                                         k['created_at'][:10])
+            buy_sum_price += (float(k['size']) * (float(k['price']) + float(k['fee'])))
+            buy_sum_size += float(k['size'])
+            buy_fee += float(k['fee'])
+        if k['product_id'] == "LTC-USD" and k['created_at'][:10] == fill_date and k['side'] == 'sell':
+            print "side: {} - size: {} * price ${:,.5f} fee: ${:,.5f} - date: {}".format(k['side'],
+                                                                                         k['size'],
+                                                                                         float(k['price']),
+                                                                                         float(k['fee']),
+                                                                                         k['created_at'][:10])
+            sell_sum_price += (float(k['size']) * (float(k['price']) + float(k['fee'])))
+            sell_sum_size += float(k['size'])
+            sell_fee += float(k['fee'])
+
+    if buy_sum_size > 0:
+        buy_avg_price = buy_sum_price / buy_sum_size
+    else:
+        buy_avg_price = 0
+    if sell_sum_size > 0:
+        sell_avg_price = sell_sum_price / sell_sum_size
+    else:
+        sell_avg_price = 0
+
+    print "\n\n"
+    print "Traded on: {}".format(fill_date)
+    print "*" * 45
+    print "Total LTC Bought: {:,.5f} \nPrice Paid: ${:,.2f} \nAverage Price Paid: ${:,.2f}".format(buy_sum_size,
+                                                                                                   buy_avg_price,
+                                                                                                   buy_avg_price)
+    print "*" * 45
+    print "Total LTC Sold: {:,.5f} \nFor a Total Of: ${:,.2f} \nAverage Price Sold: ${:,.2f}".format(sell_sum_size,
+                                                                                                     sell_avg_price,
+                                                                                                     sell_avg_price)
+    print "*" * 45
 
 
-
-
-
-
-
+if __name__ == '__main__':
+    get_current_fill(3)
 
 # Place an order
 # order = {
