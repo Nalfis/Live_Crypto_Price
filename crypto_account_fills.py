c103f168d0aba716cc5878d99b3f5c8da714c920
Nalfis Tobar <nalfis.tobar@gmail.com>
2017-10-16 20:03:55 -0400

    Merge branch 'master' of /home/naipe/PycharmProjects/Live_Crypto_Fills with conflicts.

--- crypto_account_fills.py
+++ crypto_account_fills.py
@@ -10,6 +10,8 @@ import json, hmac, hashlib, time, requests, base64, yaml
 from requests.auth import AuthBase
 
 # Create custom authentication for Exchange
+
+
 class CoinbaseExchangeAuth(AuthBase):
     def __init__(self, api_key, secret_key, passphrase):
         self.api_key = api_key
@@ -44,55 +46,17 @@ API_PASS = cfg['auth']['pass_ph']
 auth = CoinbaseExchangeAuth(API_KEY, API_SECRET, API_PASS)
 
 # Get accounts
-accounts = requests.get(api_url + 'accounts', auth=auth)
-recent_fills = requests.get(api_url + 'fills', auth=auth)
-coins = accounts.json()
-fills = recent_fills.json()
-
-print json.dumps(fills, indent=4)
-
-def buys_by_date():
-    for x, y in groupby(fills, key=lambda v: v['side']):
-        for k, v in groupby(y, key=lambda x: x['created_at'][:10]):
-            print {'date': k+'', 'fee': sum(float(d['fee'])for d in v), 'side': x}
+fills = requests.get(api_url + 'fills', auth=auth)
+list_fills = fills.json()
+print json.dumps(list_fills, indent=4, sort_keys=True)
 
 
 
 
 
-#print json.dumps(coins, indent=4, sort_keys=True)
-#print json.dumps(recent_fills, indent =4, sort_keys=True)
-
-def current_ltc():
-    for key, currency in enumerate((c['currency'], c['balance']) for c in coins):
-        if currency[0] == 'LTC':
-            global current_ltc_quant
-            current_ltc_quant = currency[1]
-        if currency[0] == 'USD':
-            current_usd = currency[1]
 
 
-def current_price():
-    while True:
-        get_LTC_price = requests.get('https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD')
-        if get_LTC_price.status_code != 200:
-            print "Something is wrong with the URL or the API-Get request"
-        else:
-            current_ltc()
-            live_LTC = get_LTC_price.json()
-            stdout.write("\rLive Price: %s Current Value of Total LTC holdings: %s" %
-                         ('${:0,.2f}'.format(live_LTC['USD']), '${:0,.2f}'.format(live_LTC['USD'] *
-                                                                                  (float(current_ltc_quant)))))
-            stdout.flush()
-            time.sleep(2)
 
-
-if __name__ == "__main__":
-    print json.dumps(recent_fills, indent=4, sort_keys=True)
-    #buys_by_date()
-    #current_price()
-    
-    
 # Place an order
 # order = {
 #     'size': 1.0,
